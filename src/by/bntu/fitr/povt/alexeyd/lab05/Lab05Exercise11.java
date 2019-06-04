989f259778e9f6d766c9a1f6d833b2e04f93dd46
Alexis-Dr <alexeydruzik@inbox.ru>
2019-06-05 00:30:32 +0300

    Lab05. Fixed exercise 11.

--- src/by/bntu/fitr/povt/alexeyd/lab05/Lab05Exercise11.java
+++ src/by/bntu/fitr/povt/alexeyd/lab05/Lab05Exercise11.java
@@ -1,12 +1,22 @@
 package by.bntu.fitr.povt.alexeyd.lab05;
 
 /**
- *
+ * Consider the following code.
+ * o A. 2
+ * o B. 3
+ * o C. 4
+ * o D. 5
+ * o E. 6
+ * o F. compilation fails
+ * o G. an exception is thrown at runtime o Your answer:
  * Answer:
+ * D. 5
  */
 public class Lab05Exercise11 {
 
     public static void main(String[] args) {
-
+        double d = 4;
+        d = d+++d---d--;
+        System.out.println(d);
     }
 }
